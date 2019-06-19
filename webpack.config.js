d0d3c5b4692bb0ef507944a57ad0015cc565a970
Michael Terranova <newfoundland7@gmail.com>
2019-06-18 18:07:43 -0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,22 @@ module.exports = {
   entry: [
     './client/index.js'
   ],
+  mode: 'development',
   output: {
     path: __dirname + '/client/dist',
     filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       { 
         test: /\.js$/, 
         exclude: /node_modules/, 
-        loader: "babel", 
-        query: { presets: ['es2015', 'react', 'stage-1'] }
+        use: {
+          loader: "babel-loader", 
+          options: {
+            presets: ['es2015', 'react', 'stage-1']
+          }
+        }
       }
     ]
   }
